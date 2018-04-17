32a86e2551fc17c22ed9cfc025f9d51e3c9cb242
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-04-17 22:45:20 +0200

    Refactor base client and USDA client

--- usda/base.py
+++ usda/base.py
@@ -1,32 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-try:
-    from urllib.parse import urlencode
-    from urllib.request import urlopen
-except ImportError:  # Python 2 compatibility
-    from urllib import urlencode
-    from urllib2 import urlopen
 
-import json
+import requests
 
-uri_base = 'http://api.data.gov/'
+BASE_URI = 'http://api.data.gov/'
 
 
-def get_response_data(uri):
-    return json.load(urlopen(uri))
+class DataGovApiError(BaseException):
+    """Base class for all Data.gov API errors"""
+    pass
+
+
+class DataGovApiRateExceededError(DataGovApiError):
+    """Data.gov API rate limit has been exceeded for this key"""
+
+    def __init__(self):
+        super(DataGovApiRateExceededError, self).__init__(
+            'API rate limit has been exceeded.')
+
+
+def api_request(uri, **parameters):
+    """Get an API response"""
+    r = requests.get(uri, parameters)
+    if r.ok:
+        return r.json()
+    try:
+        data = r.json()
+    except ValueError:  # Server did not even return a JSON for the error
+        r.raise_for_status()
+    # The JSON error data when the API rate limit is exceeded is in a
+    # different format than on parameter errors. This will handle both.
+    if data.get('errors') is not None:
+        err = data['errors']['error'][0]
+    else:  # API rate limit exceeded error format
+        err = data['error']
+    if err.get('parameter') is not None:  # Wrong parameter error
+        raise ValueError(
+            "API responded with an error on parameter '{0}': {1}".format(
+                err['parameter'], err['message']))
+    elif err['code'] == "OVER_RATE_LIMIT":
+        raise DataGovApiRateExceededError()
+    else:
+        raise DataGovApiError("{0}: {1}".format(err['code'], err['message']))
 
 
 class DataGovClientBase(object):
 
-    def __init__(self, uri_part, api_key, use_format=True):
+    def __init__(self, uri_part, api, api_key, use_format=True):
         self.uri_part = uri_part
+        self.api = api
         self.key = api_key
         self.use_format = use_format
 
-    def build_uri(self, api, uri_action, **kwargs):
+    def build_uri(self, uri_action):
+        return "{0}{1}{2}/{3}".format(
+            BASE_URI, self.uri_part, self.api.value, uri_action.value)
+
+    def run_request(self, uri_action, **kwargs):
         kwargs['api_key'] = self.key
         if 'format' not in kwargs and self.use_format:
             kwargs['format'] = 'json'
-        params = urlencode(kwargs)
-        return "{0}{1}{2}/{3}?{4}".format(
-            uri_base, self.uri_part, api, uri_action, params)
+        return api_request(self.build_uri(uri_action), **kwargs)
--- usda/client.py
+++ usda/client.py
@@ -3,44 +3,40 @@
 
 from .enums import *
 from .domain import Nutrient, Food, FoodReport
-from .base import DataGovClientBase, get_response_data
+from .base import DataGovClientBase
 
 
 class UsdaClient(DataGovClientBase):
 
     def __init__(self, api_gov_key):
-        super(UsdaClient, self).__init__('usda/', api_gov_key)
+        super(UsdaClient, self).__init__('usda/', UsdaApis.ndb, api_gov_key)
 
     def list_nutrients(self, max, offset=0, sort='n'):
-        uri = super(UsdaClient, self).build_uri(
-            UsdaApis.ndb.value, UsdaUriActions.list.value,
-            lt=UsdaNdbListType.all_nutrients.value, max=max,
-            offset=offset, sort=sort)
-        response_data = get_response_data(uri)
-        nutrients = self._build_nutrients_list(response_data)
-        return nutrients
+        data = self.run_request(
+            UsdaUriActions.list, lt=UsdaNdbListType.all_nutrients.value,
+            max=max, offset=offset, sort=sort)
+        return self._build_nutrients_list(data)
 
     def list_foods(self, max, offset=0, sort='n'):
-        uri = super(UsdaClient, self).build_uri(
-            UsdaApis.ndb.value, UsdaUriActions.list.value,
-            lt=UsdaNdbListType.food.value, max=max, offset=offset, sort=sort)
-        response_data = get_response_data(uri)
-        foods = self._build_foods_list(response_data)
-        return foods
+        data = self.run_request(
+            UsdaUriActions.list, lt=UsdaNdbListType.food.value,
+            max=max, offset=offset, sort=sort)
+        return self._build_foods_list(data)
 
     def get_food_report(self, ndb_food_id,
                         report_type=UsdaNdbReportType.basic):
-        uri = super(UsdaClient, self).build_uri(
-            UsdaApis.ndb.value, UsdaUriActions.report.value,
-            type=report_type.value, ndbno=ndb_food_id)
-        response_data = get_response_data(uri)
-        return FoodReport.from_response_data(response_data)
+        data = self.run_request(
+            UsdaUriActions.report, type=report_type.value, ndbno=ndb_food_id)
+        return FoodReport.from_response_data(data)
 
-    def get_nutrient_report(self, ndb_nutrient_id,
+    def get_nutrient_report(self, *nutrients,
                             report_type=UsdaNdbReportType.basic):
-        uri = super(UsdaClient, self).build_uri(
-            UsdaApis.ndb.value, UsdaUriActions.report.value,
-            type=report_type.value, ndbno=ndb_nutrient_id)
+        raise NotImplementedError  # TODO
+        if len(nutrients) > 20:
+            raise ValueError("A nutrient report request cannot contain "
+                             "more than 20 nutrients")
+        data = self.run_request(
+            UsdaUriActions.report, type=report_type.value, nutrients=nutrients)
 
     def _build_item_list(self, data, usda_class):
         result = list()
