8f06aa4c51a1fe49a600e84cd4b34a0812ab825f
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-04-22 15:42:53 +0200

    Add dev testing dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,18 @@ setup(
     package_data={
         '': ['*.md', 'LICENSE', 'README'],
     },
-    install_requires=['enum34>=1.1.3', 'six>=1.11.0', 'requests>=2.16.0'],
+    install_requires=[
+        'enum34>=1.1.3',
+        'six>=1.11.0',
+        'requests>=2.16.0'
+    ],
+    extras_require={
+        'dev': [
+            "pytest",
+            "pytest-cov",
+            "pytest-pep8"
+        ]
+    },
     license='GNU General Public License 3',
     description="A fork of pygov focused on USDA nutritional database API",
     long_description=open('README.md').read(),
