f09700aaa5cd5cd59108c3eb4c031483fefec2a0
Lucidiot <15960782+Lucidiot@users.noreply.github.com>
2018-07-12 08:41:22 +0200

    Drop Python 3.3 support
    I just want to write beautiful code. Why would you not use
    the latest version of Py2 or Py3 anyway?

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         "Natural Language :: English",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
