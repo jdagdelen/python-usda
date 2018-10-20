b78958c94b6d9516639d97ab4717313436d8b736
Lucidiot <lucidiot@protonmail.com>
2018-10-20 22:48:15 +0200

    Add Python 3.7 official support

--- setup.py
+++ setup.py
@@ -30,9 +30,11 @@ setup(
         "Natural Language :: English",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     project_urls={
         "Source Code": "https://gitlab.com/Lucidiot/python-usda",
