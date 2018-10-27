866b857175b9a0f519befb8d7a01ff2ddef6d5b6
Lucidiot <lucidiot@protonmail.com>
2018-10-27 15:40:54 +0200

    Update docs config

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,10 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'description': 'USDA NDB API client',
+    'fixed_sidebar': 'true',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
